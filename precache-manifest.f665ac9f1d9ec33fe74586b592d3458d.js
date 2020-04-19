01cc7f6fdba0f088fdb218a4d902d3648b032ca3
Jiří Schejbal <jirkaschej@seznam.cz>
2020-04-19 13:10:42 +0200

    Updates

---
+++ precache-manifest.f665ac9f1d9ec33fe74586b592d3458d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362aad282b840df707482d572374d526",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "d063c310c28120448310",
+    "url": "/weather-app/static/css/main.48aef760.chunk.css"
+  },
+  {
+    "revision": "8bdf28987a7fd05656ca",
+    "url": "/weather-app/static/js/2.3a97fabd.chunk.js"
+  },
+  {
+    "revision": "71b7acf5fe3ea63947bc0d9171c5b460",
+    "url": "/weather-app/static/js/2.3a97fabd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d063c310c28120448310",
+    "url": "/weather-app/static/js/main.155900a9.chunk.js"
+  },
+  {
+    "revision": "f406bccea8c3b3807745",
+    "url": "/weather-app/static/js/runtime-main.2205dcc8.js"
+  }
+]);
